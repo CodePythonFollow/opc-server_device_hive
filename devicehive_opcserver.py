6868d4fbfd1fab590b2346aa462ac78ed8e0ded4
zhengtao.gao <1284954990@qq.com>
2021-01-15 17:40:46 +0800

    demo

--- devicehive_opcserver.py
+++ devicehive_opcserver.py
@@ -61,11 +61,17 @@ class OPCScript(UASocketClient):
     # 读取配置文件
     @staticmethod
     def read_config():
+        global DeviceId, PlatformIp, Login, Password, accept_command_name
         dirs = os.listdir('./')
         if 'config.json' in dirs:
             try:
                 with open('config.json') as fi:
                     config = json.loads(fi.read())
+                DeviceId = config["DeviceId"]
+                PlatformIp = config['PlatformIp']
+                Login = config['Login']
+                Password = config['Password']
+                accept_command_name = config['accept_command_name']
                 if config.get("ip", '') and config.get("port", ""):
                     if config.get("nodes", ''):
                         OPCScript.ip = config["ip"]
@@ -74,6 +80,7 @@ class OPCScript(UASocketClient):
                         OPCScript.find_nodes = []
                         for _, value in config["nodes"].items():
                             OPCScript.find_nodes.append(value)
+                # print(DeviceId, PlatformIp, Login, Password, accept_command_name, OPCScript.find_nodes)
             except Exception as e:
                 msg = {
                     "notification": "error",
@@ -103,27 +110,26 @@ class OPCScript(UASocketClient):
             return False
 
     # 检测配置文件是否更新
-    def config_change(self):
-        dirs = os.listdir('./')
-        if 'config.json' in dirs:
-            with open('config.json') as fi:
-                config = json.loads(fi.read())
-            if config.get("ip", '') == OPCScript.ip and config.get("port", "") == OPCScript.port:
-                if config.get("nodes"):
-                    new_to_find_nodes = []
-                    for _, value in config["nodes"].items():
-                        new_to_find_nodes.append(value)
-                    if new_to_find_nodes == OPCScript.find_nodes:
-                        pass
-                    else:
-                        OPCScript.config_status = Status.CONFIG_UPDATE
-                else:
-                    OPCScript.config_status = Status.CONFIG_UPDATE
-            else:
-                OPCScript.config_status = Status.CONFIG_UPDATE
-        else:
-            OPCScript.config_status = Status.NOT_FIND
-        threading.Timer(3, self.config_change).start()
+    # def config_change(self):
+    #     dirs = os.listdir('./')
+    #     if 'config.json' in dirs:
+    #         with open('config.json') as fi:
+    #             config = json.loads(fi.read())
+    #         if config.get("ip", '') == OPCScript.ip and config.get("port", "") == OPCScript.port:
+    #             if config.get("nodes"):
+    #                 new_to_find_nodes = []
+    #                 for _, value in config["nodes"].items():
+    #                     new_to_find_nodes.append(value)
+    #                 if new_to_find_nodes == OPCScript.find_nodes:
+    #                     pass
+    #                 else:
+    #                     OPCScript.config_status = Status.CONFIG_UPDATE
+    #             else:
+    #                 OPCScript.config_status = Status.CONFIG_UPDATE
+    #         else:
+    #             OPCScript.config_status = Status.CONFIG_UPDATE
+    #     else:
+    #         OPCScript.config_status = Status.NOT_FIND
 
     # 订阅节点
     def subscribe_nodes(self):
@@ -159,87 +165,90 @@ class OPCScript(UASocketClient):
                 print("还未订阅节点")
 
     # 当收到 device hive 平台传到的命令调用此方法   传入command.command
-    def read_device_hive_msg(self):
-        while not need_achieve_commands.empty():
-            command = need_achieve_commands.get()
-            command_dic = command['parameters']
-            try:
-                data = command_dic["data"]
-                # print(data)
-                error = []
-                for node in data:
-                    node_id = node["nodeId"]
-                    value = node["value"]
-                    data_type = node["dataType"]
-                    node = OPCScript.client.get_node(node_id)
-                    if node.get_value_rank() < 0:
-                        try:
-                            if data_type == "int":
-                                value = eval(value)
-                                node.set_value(value, node.get_data_type_as_variant_type())
-                            elif data_type == "float":
-                                try:
-                                    d_value = ua.DataValue(ua.Variant(eval(value), ua.VariantType.Double))
-                                    node.set_value(d_value)
-                                except BadTypeMismatch:
-                                    # print(e)
-                                    value = ua.DataValue(ua.Variant(float(value), ua.VariantType.Float))
+    @staticmethod
+    def read_device_hive_msg():
+        while True:
+            while not need_achieve_commands.empty():
+                command = need_achieve_commands.get()
+                command_dic = command['parameters']
+                try:
+                    data = command_dic["data"]
+                    # print(data)
+                    error = []
+                    for node in data:
+                        node_id = node["nodeId"]
+                        value = node["value"]
+                        data_type = node["dataType"]
+                        node = OPCScript.client.get_node(node_id)
+                        if node.get_value_rank() < 0:
+                            try:
+                                if data_type == "int":
+                                    value = eval(value)
+                                    node.set_value(value, node.get_data_type_as_variant_type())
+                                elif data_type == "float":
+                                    try:
+                                        d_value = ua.DataValue(ua.Variant(eval(value), ua.VariantType.Double))
+                                        node.set_value(d_value)
+                                    except BadTypeMismatch:
+                                        # print(e)
+                                        value = ua.DataValue(ua.Variant(float(value), ua.VariantType.Float))
+                                        node.set_value(value)
+                                elif data_type == "string":
+                                    value = ua.DataValue(ua.Variant(value, ua.VariantType.String))
                                     node.set_value(value)
-                            elif data_type == "string":
-                                value = ua.DataValue(ua.Variant(value, ua.VariantType.String))
-                                node.set_value(value)
-                            elif data_type == "datetime":
-                                value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
-                                value = ua.DataValue(ua.Variant(value, ua.VariantType.DateTime))
-                                node.set_value(value)
-                        except Exception as e:
-                            print("错误")
-                            error.append(e)
+                                elif data_type == "datetime":
+                                    value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
+                                    value = ua.DataValue(ua.Variant(value, ua.VariantType.DateTime))
+                                    node.set_value(value)
+                            except Exception as e:
+                                print("错误")
+                                error.append(e)
+                        else:
+                            error.append("可能由于数据为数组类型引起的错误")
+                    # OPCScript.client.disconnect()
+                    if not error:
+                        result = {'result': 'Succeed'}
+                        status = 200
                     else:
-                        error.append("可能由于数据为数组类型引起的错误")
-                # OPCScript.client.disconnect()
-                if not error:
-                    result = {'result': 'Succeed'}
-                    status = 200
-                else:
-                    error_msg = ''
-                    for e in error:
-                        error_msg += 'error :' + str(e) + '\n'
-                    result = {"error": error}
-                    status = 300
-                command['result'] = result
-                command['status'] = status
-            except Exception as e:
-                command['result'] = {"error": str(e)}
-                command['status'] = 300
-            need_update_commands.put(command)
-        threading.Timer(5, self.read_device_hive_msg).start()
+                        error_msg = ''
+                        for e in error:
+                            error_msg += 'error :' + str(e) + '\n'
+                        result = {"error": error}
+                        status = 300
+                    command['result'] = result
+                    command['status'] = status
+                except Exception as e:
+                    command['result'] = {"error": str(e)}
+                    command['status'] = 300
+                need_update_commands.put(command)
+            time.sleep(5)
 
     # 每隔5秒发送一次连接状态   线程
     def exchange_connect_status(self):
-        source_times_tamp = datetime.datetime.utcnow().isoformat()
-        try:
-            # 不用同一个对象防止关闭同一个连接
-            client = Client(f"opc.tcp://{self.ip}:{self.port}/")
-            client.connect()
-            client.disconnect()
-            submit_status = 1
-        except Exception as e:
-            OPCScript.connect_status = Status.FAILED
-            submit_status = 0
-            print(e)
-        msg = {
-            "notification": 'HeatBeat',
-            "parameters": {
-                "status": submit_status,
-                "timestamp": source_times_tamp,
-                "connectType": "opc ua",
-                "processStatus": True if (threading.activeCount() >= 7 and submit_status == 1)
-                            or (threading.activeCount() >= 5 and submit_status == 0) else False
+        while True:
+            source_times_tamp = datetime.datetime.utcnow().isoformat()
+            try:
+                # 不用同一个对象防止关闭同一个连接
+                client = Client(f"opc.tcp://{self.ip}:{self.port}/")
+                client.connect()
+                client.disconnect()
+                submit_status = 1
+            except Exception as e:
+                OPCScript.connect_status = Status.FAILED
+                submit_status = 0
+                print(e)
+            msg = {
+                "notification": 'HeatBeat',
+                "parameters": {
+                    "status": submit_status,
+                    "timestamp": source_times_tamp,
+                    "connectType": "opc ua",
+                    "processStatus": True if (threading.activeCount() >= 7 and submit_status == 1)
+                                or (threading.activeCount() >= 5 and submit_status == 0) else False
+                }
             }
-        }
-        need_send_to_device_hive.put(msg)
-        threading.Timer(5, self.exchange_connect_status).start()
+            need_send_to_device_hive.put(msg)
+            time.sleep(5)
 
 
 # device hive 平台对象
@@ -259,6 +268,7 @@ class Device:
             "password": Password
         }
         response = requests.post(self.token_url, headers=headers, data=json.dumps(data))
+        # print(response.text)
         # response.
         return response.json()['accessToken']
 
@@ -276,48 +286,50 @@ class Device:
 
     # 发送消息   一直监控消息队列
     def send_notification(self):
-        while not need_send_to_device_hive.empty():
-            if need_send_to_device_hive.full():
-                print("队列满了, 清空队列。")
-                need_send_to_device_hive.queue.clear()
-                break
-            msg = need_send_to_device_hive.get()
-            print(msg)
-            try:
-                self.post_notification(msg)
-            # logging.info(msg['notification'], result.timestamp)
-            except Exception as e:
-                time.sleep(2)
-                need_send_to_device_hive.put(msg)
-                print("网络故障或者平台信息更改")
-                # need_send_to_device_hive.put(error)
-        threading.Timer(1, self.send_notification).start()
+        while True:
+            while not need_send_to_device_hive.empty():
+                if need_send_to_device_hive.full():
+                    fi.write("队列满了, 清空队列。\n")
+                    need_send_to_device_hive.queue.clear()
+                    break
+                msg = need_send_to_device_hive.get()
+                print(msg)
+                try:
+                    self.post_notification(msg)
+                # logging.info(msg['notification'], result.timestamp)
+                except Exception as e:
+                    time.sleep(2)
+                    need_send_to_device_hive.put(msg)
+                    fi.write("网络故障或者平台信息更改\n")
+                    # need_send_to_device_hive.put(error)
+            time.sleep(1)
 
     # 监控命令线程  并更新命令
     def get_commands(self):
-        try:
-            token = self.get_token()
-            url = f"http://{PlatformIp}/api/rest/device/{DeviceId}/command"
-            headers = {
-                "Authorization": f"Bearer {token}",
-                "Content-type": "application/json",
-            }
-            response = requests.get(url, headers=headers)
-            if response.json():
-                for command in response.json():
-                    if command['status'] is None:
-                        # 已接受将status置为 100
-                        command['status'] = '100'
-                        self.update_commands(command)
-                        print('get')
-                        need_achieve_commands.put(command)
-        except Exception as e:
+        while True:
+            try:
+                token = self.get_token()
+                url = f"http://{PlatformIp}/api/rest/device/{DeviceId}/command"
+                headers = {
+                    "Authorization": f"Bearer {token}",
+                    "Content-type": "application/json",
+                }
+                response = requests.get(url, headers=headers)
+                if response.json():
+                    for command in response.json():
+                        if command['status'] is None:
+                            # 已接受将status置为 100
+                            command['status'] = '100'
+                            self.update_commands(command)
+                            print('get')
+                            need_achieve_commands.put(command)
+            except Exception as e:
+                time.sleep(5)
+                print("网络故障或者平台信息更改 命令获取错误")
+            while not need_update_commands.empty():
+                command = need_update_commands.get()
+                self.update_commands(command)
             time.sleep(5)
-            print("网络故障或者平台信息更改 命令获取错误")
-        while not need_update_commands.empty():
-            command = need_update_commands.get()
-            self.update_commands(command)
-        threading.Timer(5, self.get_commands).start()
 
     # 命令执行完成后跟新
     def update_commands(self, command):
@@ -352,7 +364,6 @@ class Device:
 # 主线程激活opc服务
 def eternal():
     opc_server = OPCScript()
-    device = Device()
     # 连接服务订阅节点
     try:
         # 开启订阅线程  默认
@@ -360,14 +371,16 @@ def eternal():
         time.sleep(3)
         # 配置文件更新
         # opc_server.config_change()
+
+        device = Device()
         # 开启平台订阅线程  默认
-        device.get_commands()
+        threading.Thread(target=device.get_commands).start()
         # 开启处理命令线程  5s
-        opc_server.read_device_hive_msg()
+        threading.Thread(target=opc_server.read_device_hive_msg).start()
         # 开启平台发送线程  1s
-        device.send_notification()
+        threading.Thread(target=device.send_notification).start()
         # 开启监控连接状态  1s
-        opc_server.exchange_connect_status()
+        threading.Thread(target=opc_server.exchange_connect_status).start()
     except Exception as e:
         print(e)
 
@@ -384,7 +397,19 @@ def eternal():
             OPCScript.config_status = Status.CONFIG_NOT_CHANGE
 
         elif OPCScript.connect_status == Status.SUCCEED:
-            print("连接正常")
+            if threading.activeCount() >= 7:
+                print("连接正常")
+            else:
+                # 尝试连接平台成功表示订阅线程出错
+                try:
+                    _ = Device().get_token()
+                    fi.write("订阅线程意外终止\n")
+                    opc_server.unsubscribe_nodes()
+                    time.sleep(2)
+                    print("重启订阅")
+                    opc_server.subscribe_nodes()
+                except Exception as e:
+                    print("平台网络问题。无需重启订阅")
 
         elif OPCScript.connect_status == Status.FAILED:
             print("连接失败")
@@ -397,11 +422,13 @@ def eternal():
 
 if __name__ == '__main__':
     # 设备数据
-    DeviceId = 'r3GKj5VBtVE3WqdAxqtDzbe39pSIMBpNerAO'
-    PlatformIp = '10.159.44.180'
-    Login = 'ctqUser'
-    Password = 'program111'
-    accept_command_name = 'update'
+    DeviceId = ''
+    PlatformIp = ''
+    Login = ''
+    Password = ''
+    accept_command_name = ''
+    # 错误日志文件
+    fi = open("./error.txt", 'a')
 
     # 需要徐上传到平台的 msg opc订阅线程负责追加，device_hive上传线程负责上传
     need_send_to_device_hive = queue.Queue(maxsize=100)
